5f34f0f59db5f4a69d384658ebfa4e4f9021b660
Christian Brintnall <christianbrintnall@gmail.com>
2022-11-06 23:17:06 -0800

    init commit, baseline functionality added

---
+++ Source/EzOutfit/Entry.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+using HarmonyLib;
+using System.Reflection;
+
+[StaticConstructorOnStartup]
+public static class EzOutfit
+{
+  const string MOD_PACKAGE = "OtterBee.Ez.Outfit";
+
+  static EzOutfit()
+  {
+    Log.Message("Patching...");
+    var harmony = new Harmony(MOD_PACKAGE);
+    harmony.PatchAll(Assembly.GetExecutingAssembly());
+    Log.Message("Done");
+  }
+}
---
+++ Source/EzOutfit/OutfitDialoguePatch.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using System.Linq;
+using System.Collections.Generic;
+
+[HarmonyPatch(typeof(Dialog_ManageOutfits))]
+[HarmonyPatch(nameof(Dialog_ManageOutfits.DoWindowContents))]
+static class Dialog_ManageOutfits_OutfitDialogue_Patch 
+{
+  const float BUTTON_WIDTH = 150f;
+
+  static void Postfix()
+  {
+    Rect createRect = new Rect((BUTTON_WIDTH * 3) + 30f, 0f, BUTTON_WIDTH, 35f);
+    TextAnchor? overrideTextAnchor3 = new TextAnchor?();
+
+    var pawns = Find.ColonistBar
+      .Entries
+      .Select(entry => entry.pawn)
+      .ToArray();
+
+    if (Widgets.ButtonText(createRect, "Create from pawn", overrideTextAnchor: overrideTextAnchor3))
+    {
+      List<FloatMenuOption> options = new List<FloatMenuOption>();
+
+      foreach (Pawn pawn in pawns)
+      {
+        options.Add(
+          new FloatMenuOption(pawn.Name.ToStringShort, () => CreateOutfitFromPawn(pawn))
+        );
+      }
+
+      Find.WindowStack.Add(new FloatMenu(options));
+    }
+  }
+
+  static void CreateOutfitFromPawn(Pawn pawn)
+  {
+    Outfit createdOutfit = Current.Game.outfitDatabase.MakeNewOutfit();
+
+    createdOutfit.label = $"{pawn.Name.ToStringShort}'s outfit";
+    createdOutfit.filter.SetDisallowAll();
+
+    foreach(Apparel apparel in pawn.apparel.WornApparel)
+    {
+      createdOutfit.filter.SetAllow(apparel.def, true);
+    }
+  }
+}
